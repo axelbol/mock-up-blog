b2fbf4e6a8b0291ae4f6a3f0ff9b309518f6f8b2
axelbol <axel_lp_alvarado@hotmail.com>
2023-03-02 10:39:53 -0400

    Condensed cards & newsletter form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,8 +20,8 @@
             </div>
         </div>
     </div>
-    <div class="recently-post-container max-w-6xl mx-auto">
-        <div class="bg-gray-100 rounded-xl mt-12">
+    <div class="main-container max-w-6xl mx-auto mt-12 space-y-6">
+        <div class="recently-post-container bg-gray-100 border border-red-700 border-opacity-50 rounded-xl hover:opacity-75 transition ease-in-out duration-700">
             <div class="p-6 flex flex-row justify-between">
                 <div class="flex-1 mr-8">
                     <img src="{{ asset('img/illustration-1.png') }}" alt="illustration" class="rounded-xl">
@@ -55,6 +55,87 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </div> <!-- end recently post container -->
+        </div> <!-- end recently post container -->
+
+        <div class="secondary-post-container grid grid-cols-2">
+            @for ($i = 0; $i < 2; $i++)
+                <div class="bg-gray-100 border border-red-700 border-opacity-50 rounded-xl hover:opacity-75 transition ease-in-out duration-700">
+                    <div class="p-6">
+                        <div class="">
+                            <img src="{{ asset('img/illustration-2.png') }}" alt="illustration" class="rounded-xl">
+                        </div>
+                        <div class="flex flex-col justify-between mt-8">
+                            <div class="space-x-2">
+                                <a href="#" class="px-3 py-1 border border-blue-300 rounded-full text-blue-500 text-xs uppercase font-semibold">Techniques</a>
+                                <a href="#" class="px-3 py-1 border border-red-300 rounded-full text-red-500 text-xs uppercase font-semibold">Updates</a>
+                            </div>
+                            <div class="mt-4">
+                                <div class="text-cyan-600 tracking-wide font-semibold text-3xl text-left">This is a big title and it will look great on two or even three lines. Woohoo!</div>
+                                <span class="mt-2 block text-gray-800 text-xs">
+                                    Published <time>1 day ago</time>
+                                </span>
+                            </div>
+                            <div class="text-neutral-800 tracking-wide text-sm mt-4 text-justify">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nulla dolor velit officia quis iusto dolorum cumque voluptatum reprehenderit totalique beatae nihil ipsa! Saepe magni doloribus magnam cupiditate ipsum quis sint.</p>
+                                <p class="mt-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque dolore et eveniet saepe aliquam dolor facere maxime! Vero ratione atque ad distinctio</p>
+                            </div>
+                            <div class="flex justify-between items-center mt-8">
+                                <div class="flex items-center text-sm">
+                                    <img src="{{ asset('img/lary-avatar.svg') }}" alt="lary avatar">
+                                    <div class="ml-3 text-fuchsia-600">
+                                        <h5 class="font-bold">Lary Laracore</h5>
+                                        <h6>Mascot at Laracasts</h6>
+                                    </div>
+                                </div>
+                                <div>
+                                    <a href="#" class="text-xs font-semibold bg-blue-600 ml-3 rounded-full py-3 px-8 hover:bg-blue-700 transition ease-in-out duration-150">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endfor
+        </div> <!-- end secondary post container -->
+
+        <div class="third-post-container grid grid-cols-3">
+            @for ($i = 0; $i < 3; $i++)
+                <div class="bg-gray-100 border border-red-700 border-opacity-50 rounded-xl hover:opacity-75 transition ease-in-out duration-700">
+                    <div class="p-6">
+                        <div class="">
+                            <img src="{{ asset('img/illustration-3.png') }}" alt="illustration" class="rounded-xl">
+                        </div>
+                        <div class="flex flex-col justify-between mt-8">
+                            <div class="space-x-2">
+                                <a href="#" class="px-3 py-1 border border-blue-300 rounded-full text-blue-500 text-xs uppercase font-semibold">Techniques</a>
+                                <a href="#" class="px-3 py-1 border border-red-300 rounded-full text-red-500 text-xs uppercase font-semibold">Updates</a>
+                            </div>
+                            <div class="mt-4">
+                                <div class="text-cyan-600 tracking-wide font-semibold text-3xl text-left">This is a big title and it will look great on two or even three lines. Woohoo!</div>
+                                <span class="mt-2 block text-gray-800 text-xs">
+                                    Published <time>1 day ago</time>
+                                </span>
+                            </div>
+                            <div class="text-neutral-800 tracking-wide text-sm mt-4 text-justify">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nulla dolor velit officia quis iusto dolorum cumque voluptatum reprehenderit totalique beatae nihil ipsa! Saepe magni doloribus magnam cupiditate ipsum quis sint.</p>
+                                <p class="mt-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque dolore et eveniet saepe aliquam dolor facere maxime! Vero ratione atque ad distinctio</p>
+                            </div>
+                            <div class="flex justify-between items-center mt-8">
+                                <div class="flex items-center text-sm">
+                                    <img src="{{ asset('img/lary-avatar.svg') }}" alt="lary avatar">
+                                    <div class="ml-3 text-fuchsia-600">
+                                        <h5 class="font-bold">Lary Laracore</h5>
+                                        <h6>Mascot at Laracasts</h6>
+                                    </div>
+                                </div>
+                                <div>
+                                    <a href="#" class="text-xs font-semibold bg-blue-600 ml-3 rounded-full py-3 px-8 hover:bg-blue-700 transition ease-in-out duration-150">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endfor
+        </div> <!-- end third post container -->
+
+    </div> <!-- end main container -->
 @endsection
